31745d6a5cd8a8dd311300979f6bf2e5be42066d
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-03-30 10:05:45 +0200

    Add navbar, import Bootstrap CDN and FontAwesome

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
 <template>
   <div id="app">
+    <Navbar/>
     <ToDoList v-bind:todos="todos"/>
     <CreateTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
 
 <script>
-  import ToDoList from "./components/ToDoList";
-  import CreateTodo from "./components/CreateTodo";
+import ToDoList from './components/ToDoList'
+import CreateTodo from './components/CreateTodo'
+import Navbar from './components/navbar'
 
-  export default {
-    name: 'app',
-    components: {
-      ToDoList,
-      CreateTodo,
-    },
-    data() {
-      let returnedObject = [];
-      for (let key in localStorage) {
-        if (key.includes('todo')) {
-          let jsonString = localStorage.getItem(key);
-          returnedObject.push(JSON.parse(jsonString));
-        }
+export default {
+  name: 'app',
+  components: {
+    ToDoList,
+    CreateTodo,
+    Navbar
+  },
+  data () {
+    let returnedObject = []
+    for (let key in localStorage) {
+      if (key.includes('todo')) {
+        let jsonString = localStorage.getItem(key)
+        returnedObject.push(JSON.parse(jsonString))
       }
+    }
 
-      console.log(returnedObject)
+    console.log(returnedObject)
 
-      return {
-        todos: returnedObject
-      }
-    },
-    methods: {
-      addTodo(newTodo) {
-        localStorage.setItem('todo-' + localStorage.length, JSON.stringify(newTodo));
-        this.todos = [...this.todos, newTodo];
-      },
-    },
+    return {
+      todos: returnedObject
+    }
+  },
+  methods: {
+    addTodo (newTodo) {
+      localStorage.setItem('todo-' + localStorage.length, JSON.stringify(newTodo));
+      this.todos = [...this.todos, newTodo]
+    }
   }
+}
 </script>
 
 <style>
@@ -46,6 +49,5 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <!DOCTYPE html>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Liste</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <div class="nav-item dropdown">
+        <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+           aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle fa-2x text-dark"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Se connecter</a>
+          <a class="dropdown-item" href="#">S'inscrire</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'navbar',
+    props: ['navbar']
+  }
+</script>
+
+<style scoped>
+
+</style>
