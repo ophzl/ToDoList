0664edcbd46078b610d8bd5c1afd030831b7fc5d
Adrien larger <adriencraft34@gmail.com>
2020-03-30 17:44:40 +0200

    Page profil

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,50 +1,78 @@
 <template>
-  <div class="container">
-    <form>
-      <div class="form-row">
-        <div class="col-md-4 mb-3">
-          <div>
-            Veillez entrez votre Prénom.
-          </div>
-          <label>Prénom</label>
-          <input type="text" placeholder="Prénom" value="" required>
-        </div>
-        <div class="col-md-4 mb-3">
-          <div>
-            Veuillez entrez votre nom.
-          </div>
-          <label>Nom</label>
-          <input type="text" placeholder="Nom" value="" required>
-        </div>
-      </div>
-      <div class="form-row">
-        <div class="col-md-6 mb-3">
-          <div>
-            Veuillez saissir votre ville.
-          </div>
-          <label>Ville</label>
-          <input type="text" placeholder="Ville" required>
-        </div>
-        <div class="col-md-3 mb-3">
-          <div>
-            Veuillez saissir votre Pays.
-          </div>
-          <label>Pays</label>
-          <input type="text" placeholder="Pays" required>
-          <div class="col-md-3 mb-3">
-            <div>
-              Veuillez saissir votre Code Postal.
+<div class="container">
+	<div class="row">
+        
+        
+       <div class="col-md-10 ">
+
+<div class="panel panel-default">
+  <div class="panel-heading">  <h4 >Profil</h4></div>
+   <div class="panel-body">
+       
+    <div class="box box-info">
+        
+            <div class="box-body">
+                     <div class="col-sm-4">
+                     <div  align="center"> <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive"> 
+                     </div>
             </div>
-            <label>Code Postal</label>
-            <input type="text" placeholder="Code Postal" required>
-          </div>
-        </div>
-        <button class="btn btn-primary" type="submit">Submit</button>
-      </div>
-    </form>
-  </div>
+            <div class="col-sm-7">
+            <h4 style="color:#00b1b1;">Brice Lasagne</h4>
+              <span><p>Developpeur/Designer</p></span>            
+            </div>
+            <div class="clearfix"></div>
+            <hr style="margin:5px 0 5px 0;">
+    
+              
+<div class="col-sm-4 col-xs-6 tital " >Prénom:</div><div class="col-sm-7 col-xs-6 ">Brice</div>
+     <div class="clearfix"></div>
+<div class="bot-border"></div>
+
+<div class="col-sm-4 col-xs-6 tital " >Nom:</div><div class="col-sm-7"> Lasagne</div>
+  <div class="clearfix"></div>
+<div class="bot-border"></div>
+
+<div class="col-sm-4 col-xs-6 tital " >Date d'inscription:</div><div class="col-sm-7">15 Jun 2017</div>
+
+  <div class="clearfix"></div>
+<div class="bot-border"></div>
+<div class="col-sm-4 col-xs-6 tital " >Derniére connexion le :</div><div class="col-sm-7">11 Jun 1999</div>
+
+  <div class="clearfix"></div>
+<div class="bot-border"></div>
+
+            </div>
+          </div>      
+        </div> 
+     </div>
+   </div> 
+	</div>
+      <div class="col-md-10 ">
+    <div class="panel-heading">  <h4 >Mes taches</h4></div>
+ </div> 
+<div class="col-sm-4 col-xs-6 tital " >Nom:</div><div class="col-sm-7">Status</div>
+
+  <div class="clearfix"></div>
+<div class="bot-border"></div>
+
+
+
+</div>
+
 </template>
 
-<style scoped>
+<style>
+              input.hidden {
+    position: absolute;
+    left: -9999px;
+}
 
+#profile-image1 {
+    cursor: pointer;
+  
+     width: 100px;
+    height: 100px;
+	border:2px solid #03b1ce ;}
+	.tital{ font-size:16px; font-weight:500;}
+	 .bot-border{ border-bottom:2px #f8f8f8 solid;  margin:5px 0  5px 0}
 </style>
