0cde46d336e444ef5aca454416da521e0c62f675
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-03-30 16:31:27 +0200

    Add route for Home.vue component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from '../App'
+// import App from '../App'
 import TodoList from '../components/TodoList'
 import Profile from '../components/Profile'
+import Home from '../components/Home'
 
 Vue.use(Router)
 
@@ -11,7 +12,7 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: App
+      component: Home
     }, {
       path: '/todolist',
       name: 'todolist',
