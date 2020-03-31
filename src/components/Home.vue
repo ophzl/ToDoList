05c44f38f170288c92f59e427c85e4bb4cd9539a
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-03-31 12:03:46 +0200

    Create home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,65 @@
 <template>
-    <div class="container">
-
+  <div>
+    <div class="mx-auto title">
+      <h1>ToDoList</h1>
+      <p>Créez et partagez vos tâches en un clic !</p>
+      <div class="card mx-auto">
+        <h3>Tâches récemment ajoutées</h3>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>/* eslint-disable */
-  export default {
-    name: 'home'
+import Todos from './Todos'
+
+export default {
+  name: 'home',
+  props: ['home'],
+  components: {
+    Todos
+  },
+  data () {
+    let returnedObject = []
+    for (let key in localStorage) {
+      if (key.includes('todo')) {
+        let jsonString = localStorage.getItem(key)
+        returnedObject.push(JSON.parse(jsonString))
+      }
+    }
+
+    return {
+      todos: returnedObject
+    }
   }
+}
 </script>
 
 <style scoped>
+  .title {
+    padding-top: 10rem;
+  }
 
+  h1 {
+    font-size: 60px;
+  }
+
+  p {
+    font-size: 40px;
+    opacity: 60%;
+    padding-top: 2rem;
+  }
+
+  .card {
+    background: #eeeeee;
+    width: 95%;
+    margin-top: 13rem;
+    height: 20rem;
+    border: none;
+  }
+
+  h3 {
+    text-align: left;
+    padding: 2rem;
+  }
 </style>
