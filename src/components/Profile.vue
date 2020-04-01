25112e9b5813b6024cd6aadaf3d072d5ef54c4b5
Adrien larger <adriencraft34@gmail.com>
2020-04-01 09:57:25 +0200

    Nouvelle_page_utilisateur

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -3,54 +3,86 @@
 	<div class="row">
         
         
-       <div class="col-md-10 ">
+       <div class="col-md-11 ">
 
-<div class="panel panel-default">
-  <div class="panel-heading">  <h4 >Profil</h4></div>
-   <div class="panel-body">
+<div class="card ">
+  <div class="card-heading text-white bg-primary">  <h4>Profile</h4></div>
+   <div class="card-body">
        
     <div class="box box-info">
         
-            <div class="box-body">
-                     <div class="col-sm-4">
-                     <div  align="center"> <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" class="img-circle img-responsive"> 
+        <div class="box-body">
+           <div class="col-sm-">
+                     <div  align="center"> <img alt="User Pic" src='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Side&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Pale' id="profile-image1" class="rounded-circle img-responsive"> 
                      </div>
             </div>
-            <div class="col-sm-7">
+            <div class="col--1 ">
             <h4 style="color:#00b1b1;">Brice Lasagne</h4>
               <span><p>Developpeur/Designer</p></span>            
             </div>
             <div class="clearfix"></div>
-            <hr style="margin:5px 0 5px 0;">
+            <hr style="margin:6px 0 9px 0;">
     
               
-<div class="col-sm-4 col-xs-6 tital " >Prénom:</div><div class="col-sm-7 col-xs-6 ">Brice</div>
-     <div class="clearfix"></div>
-<div class="bot-border"></div>
+<div class="  col-12 tital" >Name:</div><div class="col order-0 tital">Brice</div>
 
-<div class="col-sm-4 col-xs-6 tital " >Nom:</div><div class="col-sm-7"> Lasagne</div>
-  <div class="clearfix"></div>
-<div class="bot-border"></div>
+  <div class="clearfix bot-border"></div>
 
-<div class="col-sm-4 col-xs-6 tital " >Date d'inscription:</div><div class="col-sm-7">15 Jun 2017</div>
+<div class="col-12 tital " >Last Name:</div><div class="col order-12"> Lasagne</div>
 
-  <div class="clearfix"></div>
-<div class="bot-border"></div>
-<div class="col-sm-4 col-xs-6 tital " >Derniére connexion le :</div><div class="col-sm-7">11 Jun 1999</div>
+  <div class="clearfix bot-border"></div>
 
-  <div class="clearfix"></div>
-<div class="bot-border"></div>
+<div class="col-12 tital " >Joined:</div><div class="col order-12">15 mars 2017</div>
+
+  <div class="clearfix bot-border"></div>
+
+<div class="col-12 tital " >Last connection:</div><div class="col order-12">31 mars 2020</div>
 
             </div>
           </div>      
         </div> 
-     </div>
+     </div><br>
+         <button href='#'  type="button" class="btn btn-outline-primary">Edit</button><br>
    </div> 
-	</div>
-      <div class="col-md-10 ">
-    <div class="panel-heading">  <h4 >Mes taches</h4></div>
- </div> 
-<div class="col-sm-4 col-xs-6 tital " >Nom:</div><div class="col-sm-7">Status</div>
+	</div><br>
+
+      
+    <div class="card-heading text-white bg-primary col-md-11 ">  <h4 >Tasks</h4></div>
+
+<div class="table-responsive col-md-11">
+  <table class="table ">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Create:</th>
+      <th scope="col">End date:</th>
+      <th scope="col">Priority</th>
+      <th scope="col">Archive</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Feuille impots</td>
+      <td>31/03/2020</td>
+      <td>25/04/2020</td>
+      <td>Haute</td>
+      <td class="align-middle" style="text-align: center">
+          <button type="button" class="btn btn-outline-info btn-sm m-0 ">Archive</button></td>
+    <tr>
+      <th scope="row">2</th>
+      <td>Rendre CRA</td>
+      <td>20/04/2020</td>
+      <td>30/04/2020</td>
+      <td>Basse</td>
+       <td class="align-middle" style="text-align: center">
+          <button type="button" class="btn btn-outline-info btn-sm m-0 waves-effect">Archive</button></td>
+    </tr>
+  </tbody>
+  </table>
+</div>
 
   <div class="clearfix"></div>
 <div class="bot-border"></div>
@@ -62,17 +94,12 @@
 </template>
 
 <style>
-              input.hidden {
-    position: absolute;
-    left: -9999px;
-}
-
 #profile-image1 {
     cursor: pointer;
-  
-     width: 100px;
-    height: 100px;
+    
+     width: 110px;
+    height: 110px;
 	border:2px solid #03b1ce ;}
 	.tital{ font-size:16px; font-weight:500;}
-	 .bot-border{ border-bottom:2px #f8f8f8 solid;  margin:5px 0  5px 0}
+	 .bot-border{ border-bottom:2px #f8f8f8 solid;  margin:7px 0  5px 0}
 </style>
