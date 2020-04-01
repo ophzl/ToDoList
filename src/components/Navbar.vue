cccfc86d42c97bd83425f4121e983a686e94b907
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-04-01 15:42:45 +0200

    Remove search form

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,10 +18,6 @@
           <router-link class="nav-link" to="/users" v-if="user">Gestion des utilisateurs</router-link> <!-- Ajouter rôle administrateur -->
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-light my-2 my-sm-0 text-light" type="submit">Search</button>
-      </form>
       <div class="nav-item dropdown">
         <a class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown"
            aria-haspopup="true" aria-expanded="false">
