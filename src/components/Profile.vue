750955d4dd00b348d6e4df877a6a5f96f37bdcd7
Ophelie ZEITEL <ophelie.zeitel@viacesi.fr>
2020-04-02 09:36:34 +0200

    Improve profile page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,56 +5,55 @@
       <div class="card">
         <div class="card-body">
           <div class="col--1 ">
-            <h4>{{ user }}</h4>
+            <h4>{{ user.displayName }}</h4>
             <h5></h5>
-            <span>{{ user }}</span>
+            <span>{{ user.email }}</span>
           </div>
           <hr>
           <div class="col-12 tital ">Date de première connexion :</div>
-          <div class="col order-12"></div>
+          <div class="col order-12">{{ creationTime }}</div>
           <div class="clearfix bot-border"></div>
           <div class="col-12 tital ">Date de dernière connexion :</div>
-          <div class="col order-12"></div>
+          <div class="col order-12">{{ lastSignInTime }}</div>
         </div>
       </div>
 
       <!--    User's tasks list -->
-      <div class="table-responsive pt-4">
-        <table class="table">
-          <thead>
-          <tr>
-            <th scope="col">Titre</th>
-            <th scope="col">Description</th>
-            <th scope="col">Date de fin</th>
-            <!--          <th scope="col">Priorité</th>-->
-            <th scope="col">Statut</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="task in returnedObject" v-bind:key="task.id">
-            <td>{{ task.title }}</td>
-            <td v-if="task.description">{{ task.description }}</td>
-            <td v-else-if="!task.description">Aucune description renseignée</td>
-            <td v-if="task.remindDate">{{ task.remindDate }}</td>
-            <td v-else-if="!task.remindDate">Aucune date renseignée</td>
-            <!--          <td></td>-->
-            <td class="align-middle" style="text-align: center">
-              <span class="text-warning" v-if="task.archived">Archivée</span>
-              <span class="text-success" v-if="task.done" v-show="!task.archived">Terminée</span>
-              <span class="text-danger" v-if="!task.done" v-show="!task.archived">En attente</span>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
+<!--      <div class="table-responsive pt-4">-->
+<!--        <table class="table">-->
+<!--          <thead>-->
+<!--          <tr>-->
+<!--            <th scope="col">Titre</th>-->
+<!--            <th scope="col">Description</th>-->
+<!--            <th scope="col">Date de fin</th>-->
+<!--            &lt;!&ndash;          <th scope="col">Priorité</th>&ndash;&gt;-->
+<!--            <th scope="col">Statut</th>-->
+<!--          </tr>-->
+<!--          </thead>-->
+<!--          <tbody>-->
+<!--          <tr v-for="task in returnedObject" v-bind:key="task.id">-->
+<!--            <td>{{ task.title }}</td>-->
+<!--            <td v-if="task.description">{{ task.description }}</td>-->
+<!--            <td v-else-if="!task.description">Aucune description renseignée</td>-->
+<!--            <td v-if="task.remindDate">{{ task.remindDate }}</td>-->
+<!--            <td v-else-if="!task.remindDate">Aucune date renseignée</td>-->
+<!--            &lt;!&ndash;          <td></td>&ndash;&gt;-->
+<!--            <td class="align-middle" style="text-align: center">-->
+<!--              <span class="text-warning" v-if="task.archived">Archivée</span>-->
+<!--              <span class="text-success" v-if="task.done" v-show="!task.archived">Terminée</span>-->
+<!--              <span class="text-danger" v-if="!task.done" v-show="!task.archived">En attente</span>-->
+<!--            </td>-->
+<!--          </tr>-->
+<!--          </tbody>-->
+<!--        </table>-->
+<!--      </div>-->
     </div>
   </div>
 
 </template>
 
 <script>/* eslint-disable */
-import Todo from './Todo'
-import Todos from './Todos'
+import Firebase from 'firebase'
 import db from '../../static/js/db'
 
 
@@ -62,15 +61,17 @@ export default {
   name: 'Profile',
   props: ['user'],
   components: {
-    Todo,
-    Todos,
   },
   data () {
-    db.collection('tasks')
-    .get()
+    // db.collection('users')
+    // .get(this.user.uid)
+
+    let lastSignInTime = Firebase.auth().currentUser.metadata.lastSignInTime
+    let creationTime = Firebase.auth().currentUser.metadata.creationTime
 
     return {
-      returnedObject,
+      lastSignInTime,
+      creationTime
     }
   },
 }
